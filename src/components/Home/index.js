54d0a84161c041a958c620ad7536449c3fca3602
Katarina-Slotova <katarina.slotova@gmail.com>
2024-09-17 10:49:48 -0400

    Add footer.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import Navbar from "../Navbar";
-import Intro from "../Intro";
-import SecondSection from "../SecondSection";
-import InterviewGuideSection from "../InterviewGuideSection";
-import JobSearchGuideSection from "../JobSearchGuideSection";
-import NetworkingSection from "../NetworkingSection";
-import AgenciesSection from "../AgenciesSection";
+import React from 'react'
+import Navbar from '../Navbar'
+import Intro from '../Intro'
+import SecondSection from '../SecondSection'
+import InterviewGuideSection from '../InterviewGuideSection'
+import JobSearchGuideSection from '../JobSearchGuideSection'
+import NetworkingSection from '../NetworkingSection'
+import AgenciesSection from '../AgenciesSection'
+import Footer from '../Footer'
 
 function Home() {
   return (
@@ -17,8 +18,9 @@ function Home() {
       <JobSearchGuideSection />
       <NetworkingSection />
       <AgenciesSection />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
